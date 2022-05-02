3455638a42195944ae0de6668280c4bd2bcb6c0e
Burton <shopmage@yandex.ru>
2022-05-02 14:11:35 +0300

    api view drf

--- order/views.py
+++ order/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
 from order.models import SalesOrder
+from order.serializers import OrderSerializer
 
 
 def orders_page(request):
     return render(request, 'index.html', {'orders': SalesOrder.objects.all()})
+
+
 # Create your views here.
+class OrderView(ModelViewSet):
+    queryset = SalesOrder.objects.all()
+    serializer_class = OrderSerializer
